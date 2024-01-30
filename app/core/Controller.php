cb4d03f1bdb2b0926347919ae1a197eeec705656
Ryxena <rubbenme@proton.me>
2024-01-30 14:52:03 +0700

    Updating Controller

--- app/core/Controller.php
+++ app/core/Controller.php
@@ -4,5 +4,9 @@ class Controller{
     public function view($view, $data = []) {
         require_once('../app/view/' . $view . '.php');
     }
+    public function model($model) {
+        require_once('../app/model/'. $model . '.php');
+        return new $model;
+    }
 }
 ?>
